cc5b0848993bbcb4f3330a08b80b49bd0e16877b
Reetesh Kumar <101452588+Virous77@users.noreply.github.com>
2022-06-08 09:40:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+document.querySelector(".first-button").addEventListener("click", () => {
+  document.querySelector(".first-hide").classList.toggle("hidden");
+});
+
+document.querySelector(".second-button").addEventListener("click", () => {
+  document.querySelector(".second-hide").classList.toggle("hidden");
+});
+
+document.querySelector(".third-button").addEventListener("click", () => {
+  document.querySelector(".third-hide").classList.toggle("hidden");
+});
+
+document.querySelector(".fourth-button").addEventListener("click", () => {
+  document.querySelector(".fourth-hide").classList.toggle("hidden");
+});
+/////////////////////////////////
+document.querySelector(".first-button").addEventListener("click", () => {
+  document.querySelector(".first-head").classList.toggle("heads");
+});
+
+document.querySelector(".first-button").addEventListener("click", () => {
+  document.querySelector(".first-button").classList.toggle("buttons");
+});
+/////////////////////
+document.querySelector(".second-button").addEventListener("click", () => {
+  document.querySelector(".second-head").classList.toggle("heads");
+});
+
+document.querySelector(".second-button").addEventListener("click", () => {
+  document.querySelector(".second-button").classList.toggle("buttons");
+});
+///////////////////
+document.querySelector(".third-button").addEventListener("click", () => {
+  document.querySelector(".third-head").classList.toggle("heads");
+});
+
+document.querySelector(".third-button").addEventListener("click", () => {
+  document.querySelector(".third-button").classList.toggle("buttons");
+});
+////////////////
+
+document.querySelector(".fourth-button").addEventListener("click", () => {
+  document.querySelector(".fourth-head").classList.toggle("heads");
+});
+
+document.querySelector(".fourth-button").addEventListener("click", () => {
+  document.querySelector(".fourth-button").classList.toggle("buttons");
+});
